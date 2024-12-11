b7ad31754bb10b80a5c9832dbeb7e13f611eaeac
Kroese <kroese@users.noreply.github.com>
2024-12-11 21:49:01 +0100

    Update WriterPipeline.h

--- src/WriterPipeline.h
+++ src/WriterPipeline.h
@@ -73,7 +73,7 @@ struct WriterPipeline {
                         parseAndVerifyEvent(m.eventJson, secpCtx, verifyMsg, verifyTime, packedStr, jsonStr);
                     } catch (std::exception &e) {
                         if (verboseReject) {
-                            jsonStr = std::string(m.eventJson.get_string());
+                            jsonStr = std::string(m.eventJson);
                             jsonStr = jsonStr.substr(0,200);
                             LW << "Rejected event: " << jsonStr << " reason: " << e.what();
                         }
