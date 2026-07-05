da38ba29120fb8a0b5bd0baa7790d33fab43085a
Kroese <kroese@users.noreply.github.com>
2026-07-05 23:38:33 +0200

    feat: Refactor entrypoint

--- strfry.sh
+++ strfry.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-: ${ROUTER:=''}
-: ${STREAMS:=''}
-: ${COMPACT:=''}
+: "${ROUTER:=''}"
+: "${STREAMS:=''}"
+: "${COMPACT:=''}"
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 config="/etc/strfry.conf"
 template="/etc/strfry.conf.default"
 
-# Check if config file is not a directory
-if [ -d "$config" ]; then
+prepareConfig() {
+
+  # Check if config file is not a directory
+  if [ -d "$config" ]; then
 
     echo "The bind $config maps to a file that does not exist!"
     exit 1
 
-fi
-
-if [ ! -f "$config" ]; then
-
-  if [ ! -f "$template" ]; then
-    echo "Your /etc directory does not contain a valid strfry.conf file!"
-    exit 1
   fi
 
-  cp "$template" "$config"
+  if [ ! -f "$config" ]; then
 
-fi
+    if [ ! -f "$template" ]; then
+      echo "Your /etc directory does not contain a valid strfry.conf file!"
+      exit 1
+    fi
 
-cd /app
+    cp "$template" "$config"
 
-if [[ "$COMPACT" == [Yy1]* ]]; then
+  fi
+}
+
+compactDatabase() {
+  local db lock
+
+  if [[ "$COMPACT" != [Yy1]* ]]; then
+    return 0
+  fi
 
   db="./strfry-db/data.mdb"
   lock="./strfry-db/lock.mdb"
@@ -43,28 +49,46 @@ if [[ "$COMPACT" == [Yy1]* ]]; then
   else
     echo "Error: database file $db not found.."
   fi
+}
 
-fi
+startRelay() {
+  ./strfry relay &
+  PID=$!
+}
 
-./strfry relay &
-PID=$!
+startRouter() {
 
-if [ -f "$ROUTER" ]; then
-  sleep 2
-  ./strfry router "$ROUTER" &
-fi
-
-if [[ "$ROUTER" == [Yy1]* ]]; then
-  sleep 2
-  ./strfry router /etc/strfry-router.conf &
-fi
-
-for i in $(echo "$STREAMS" | sed "s/,/ /g")
-do
-  if [[ -n "$i" ]]; then
+  if [ -f "$ROUTER" ]; then
     sleep 2
-    ./strfry stream wss://$i --dir down 2> /dev/null &
+    ./strfry router "$ROUTER" &
   fi
-done
+
+  if [[ "$ROUTER" == [Yy1]* ]]; then
+    sleep 2
+    ./strfry router /etc/strfry-router.conf &
+  fi
+}
+
+startStreams() {
+  local i
+
+  for i in $(echo "$STREAMS" | sed "s/,/ /g")
+  do
+    if [[ -n "$i" ]]; then
+      sleep 2
+      ./strfry stream wss://$i --dir down 2> /dev/null &
+    fi
+  done
+}
+
+prepareConfig
+
+cd /app
+
+compactDatabase
+
+startRelay
+startRouter
+startStreams
 
 wait "$PID"
